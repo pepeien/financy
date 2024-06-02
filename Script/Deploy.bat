5097f579878335884b20912073669655aaa9d998
pepeien <erickfcaldeira@gmail.com>
2024-06-02 00:48:03 -0300

    Improved deploy scripts

--- Script/Deploy.bat
+++ Script/Deploy.bat
@@ -1,3 +1,9 @@
 @echo off
 
+del 
+
 ..\Bin\Deployer\CQtDeployer.exe -confFile ../Deploy.json
+
+powershell Compress-Archive ../Bin/Deploy/* ../Bin/financy-win-64x.zip -Force
+
+rmdir /s /q "../Bin/Deploy"
--- Script/Deploy.sh
+++ Script/Deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 ..\Bin\Deployer\CQtDeployer -confFile ../Deploy.json
+
+tar -czvf financy-linux-64x.tar.gz ..\Bin\Deploy\*
+
+rm -r ..\Bin\Deploy
