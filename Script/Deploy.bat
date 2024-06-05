6054c905510bec004141f997652304322e6ae8b9
pepeien <erickfcaldeira@gmail.com>
2024-06-05 00:03:14 -0300

    Changed deploy logic

--- Script/Deploy.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-"../Bin/Deployer/CQtDeployer.exe" -confFile "../Deploy.json"
-powershell Compress-Archive "../Bin/Deploy/*" "../Bin/win-64x.zip" -Force
-rmdir /s /q "../Bin/Deploy"
--- Script/Deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-"../Bin/Deployer/CQtDeployer" -confFile ../Deploy.json
-zip -r ../Bin/linux-64x.zip ../Bin/Deploy/*
-rm -r ../Bin/Deploy
