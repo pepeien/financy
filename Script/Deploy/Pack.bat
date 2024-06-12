ded2bd96e5aa4ea534f9460e512be410e1435eaa
pepeien <erickfcaldeira@gmail.com>
2024-06-12 02:52:19 -0300

    Improved deploy tools

--- Script/Deploy/Pack.bat
+++ Script/Deploy/Pack.bat
@@ -1,8 +1,17 @@
 @echo off
 
-CQtDeployer -confFile "./Windows/Config.json"
+### Building
+cmake "../../" -B "../../build" -G "Visual Studio 16 2019" ^
+-DCMAKE_BUILD_TYPE="Release"
+
+cmake --build "../../build" --target ALL_BUILD --config "Release"
+
+### Packing
+CQtDeployer -confFile ".\Windows\Config.json"
 msbuild ./Windows/Packer/Windows.wixproj /property:Configuration=Release
-rmdir /s /q "..\..\Deploy/win-64x"
+
+### Clean up
+rmdir /s /q "..\..\Deploy\win-64x"
 copy ".\Windows\Packer\bin\Release\Windows.msi" "..\..\Deploy\win-64x.msi"
 
 @echo on
