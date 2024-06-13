31bea3429ee0f1ffe24c6e1fdc69e93aa03520a1
pepeien <erickfcaldeira@gmail.com>
2024-06-12 22:24:55 -0300

    Bumped app version

--- Script/Deploy/Pack.bat
+++ Script/Deploy/Pack.bat
@@ -1,16 +1,16 @@
 @echo off
 
-### Building
+REM Building
 cmake "../../" -B "../../build" -G "Visual Studio 16 2019" ^
 -DCMAKE_BUILD_TYPE="Release"
 
 cmake --build "../../build" --target ALL_BUILD --config "Release"
 
-### Packing
+REM Packing
 CQtDeployer -confFile ".\Windows\Config.json"
 msbuild ./Windows/Packer/Windows.wixproj /property:Configuration=Release
 
-### Clean up
+REM Clean up
 rmdir /s /q "..\..\Deploy\win-64x"
 copy ".\Windows\Packer\bin\Release\Windows.msi" "..\..\Deploy\win-64x.msi"
 
